e1fdfa2ef3ec75d3b23aea4ef146a0b148af3b86
aqbiak <92927640+aqbiak@users.noreply.github.com>
2021-11-19 19:14:08 +0100

    Add files via upload

---
+++ FilmManager.cs
@@ -0,0 +1,41 @@
+﻿using FilmDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB
+{
+    public class FilmManager
+    {
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            return this;
+        }
+
+        public FilmManager GetFilm(int id)
+        {
+            return null;
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            return null;
+        }
+    }
+}
